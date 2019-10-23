d08e913ce7dc6bef2ffcad2518c4ab1e08fb8551
Praneeth Bojja <54765847+Praneeth9985@users.noreply.github.com>
2019-10-22 21:03:49 -0700

    Add files via upload

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,242 +1,234 @@
 /*eslint-env browser*/
-var primaryColor = "#589ACE";
-var secondary1 = "#D2DEEE";
-var secondary2 = "#EAEFF5";
-var employee1 = ["Steve Rogers", "Team Lead", 3424];
-var employee2 = ["Tony Stark", "Sales", 3423];
-var employee3 = ["Bruce Banner", "R&D", 3425];
-var employee4 = ["Stephens Strange", "Quality Assurance", 3426];
-var employee5 = ["Thor Odinson", "Human Resource", 3427];
+var employee1 = ["Sponge Bob", "Team Lead", 3982];
+var employee2 = ["Squidward", "Sales", 3423];
+var employee3 = ["Sandy", "R&D", 3232];
+var employee4 = ["Patrick", "Quality Assurance", 3716];
+var employee5 = ["Mr. Krabbs", "CEO ", 3417];
 
+var labels = ["Name", "Title", "Extension"];
 var employees = [employee1, employee2, employee3, employee4, employee5];
-var employeeCount = employees.length;
 
-var inputs = ["Name", "Title", "Extension"];
+var $ = function(id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
 
-function $(id) {
-  return document.getElementById(id);
-}
+window.onload = function(){
+    document.title = "The Employee Managemment Application";
+    document.body.style.font = "sans-serif";
 
-window.onload = function () {
-  document.title = "The Employee Management Application";
-  document.body.style.font = "bold 14px arial, sans-serif";
+    document.body.addEventListener('click', function (event) {
+        for (i = 0; i< employees.length; i++) {
+          if( event.srcElement.id == employees[i][2]+" btn" ) {
+            deleteEmp(i, employees[i][2]);
+          }
+        }
+    });
 
-// workaround to dynamically adding listeners when dynamically creating buttons for employees table
-  document.body.addEventListener('click', function ( event) {
-    for (i = 0; i< employees.length; i++) {
-      if( event.srcElement.id == employees[i][2]+" btn" ) {
-        deleteEmployee(i, employees[i][2]);
-      }
-    }
-});
+    var heading = document.createElement("h4");
+    heading.innerHTML = "The Employee Management Application";
+    heading.style.font = "24px sans-serif";
+    document.body.appendChild(heading);
 
-  var appHeader = document.createElement("p");
-  appHeader.innerHTML = "The Employee Management Application";
-  appHeader.style.font = "16px arial";
-  document.body.appendChild(appHeader);
+    var addEmp = document.createElement("p");
+    addEmp.innerHTML = "Add Employee";
+    addEmp.style.font = "bold 18px sans-serif";
+    document.body.appendChild(addEmp);
 
-  var addEmployeeText = document.createElement("p");
-  addEmployeeText.id = "addemployeeText";
-  addEmployeeText.innerHTML = "Add Employee";
-  document.body.appendChild(addEmployeeText);
+    readInputs();
 
-  createInputs();
-
-  // for error handling
-  var errorDiv = document.createElement("div");
+    var errorDiv = document.createElement("div");
   errorDiv.id = "errorDiv";
   errorDiv.style.color = "red";
   errorDiv.style.clear = "both";
   errorDiv.style.margin = "10px 0";
   document.body.appendChild(errorDiv);
 
-  for (index = 0; index<inputs.length; index++) {
+  for (index = 0; index<labels.length; index++) {
     var error = document.createElement("p");
-    error.id = inputs[index]+"Error";
+    error.id = labels[index]+"Error";
     error.style.color = "red";
     $("errorDiv").appendChild(error);
   }
 
-  createEmptyTable();
-  displayEmployeesTable();
+    showEmptext(); 
+    createTable();
+    
+    displayTableData();
 }
 
-function createInputs() {
-  var formDiv = document.createElement("div");
-  formDiv.id = "formDiv";
-  formDiv.style.minWidth = "200px";
-  formDiv.style.width = "30%";
-  formDiv.style.maxWidth = "50%";
-  formDiv.style.display = "block";
-  document.body.appendChild(formDiv);
+function readInputs(){                                  //Inputs of the form
+    var dform = document.createElement("div");
+    dform.style.minWidth = "200px";
+    dform.style.marginTop = "25px";
+    dform.style.width = "25%";
+    document.body.appendChild(dform);
+  
+    for (i = 0; i<labels.length; i++) {
+      var inputs = document.createElement("div");
+      inputs.style.clear = "both";
+  
+      var textLabel = document.createElement("label");    
+      textLabel.style.float = "left";
+      textLabel.style.font = "16px sans-serif";
+      textLabel.innerHTML = labels[i] + ":";
+  
+      var inputBox = document.createElement("input");
+      inputBox.type = "text";
+      inputBox.id = "label"+[i];
+      inputBox.style.float = "right";
+      inputBox.style.width = "70%";
+      inputBox.style.height = "20px";
+      inputBox.style.marginBottom = "20px";
+      inputBox.style.border = "1px solid #0a718a";
+  
+      inputs.appendChild(textLabel);
+      inputs.appendChild(inputBox);
+  
+      dform.appendChild(inputs);
+    }
+    var dadd = document.createElement("div");
+    dadd.style.clear = "right";
+    dadd.align = "right";
+    var addButton = document.createElement("button");
+    addButton.innerHTML = "Add";
+    addButton.style.color = "white";
+    addButton.style.background = "#408dc7";
+    addButton.style.border ="solid #287fc0";
+    addButton.style.padding = "5px 30px";
 
-  // looping to add inputs
-  for (i = 0; i<inputs.length; i++) {
-    var inputDiv = document.createElement("div");
-    inputDiv.style.clear = "both";
 
-    var label = document.createElement("label");
-    label.style.float = "left";
-    label.style.font = "14px arial";
-    label.innerHTML = inputs[i] + ":";
-    label.for = inputs[i]+"Input";
-
-    var input = document.createElement("input");
-    input.id = inputs[i]+"Input";
-    input.type = "text";
-    input.style.float = "right";
-    input.style.width = "60%";
-    input.style.marginBottom = "10px";
-    input.style.border = "1px solid " + primaryColor;
-
-    inputDiv.appendChild(label);
-    inputDiv.appendChild(input);
-
-    formDiv.appendChild(inputDiv);
-  }
-
-  var submitDiv = document.createElement("div");
-  submitDiv.style.clear = "right";
-  submitDiv.align = "right";
-  var submitBtn = document.createElement("button");
-  submitBtn.innerHTML = "ADD";
-  submitBtn.style.color = "white";
-  submitBtn.style.background = primaryColor;
-  submitBtn.style.padding = "3px 20px";
-  submitBtn.addEventListener("click", addEmployee);
-  submitDiv.appendChild(submitBtn);
-
-  formDiv.appendChild(submitDiv);
+    addButton.addEventListener("click", addEmp);
+    dadd.appendChild(addButton);
+    dform.appendChild(dadd);
+} 
+function showEmptext(){
+    var showEmpText = document.createElement("p");
+    showEmpText.innerHTML = "Showing " +employees.length + " Employees";
+    showEmpText.style.font = "bold 14px sans-serif";
+    document.body.appendChild(showEmpText);
 }
-
-function createEmptyTable() {
-  var tableDiv = document.createElement("div");
-  tableDiv.style.float = "left";
-  tableDiv.style.minWidth = "400px";
-  tableDiv.style.width = "50%";
-  document.body.appendChild(tableDiv);
-  var showingEmployeeText = document.createElement("p");
-  showingEmployeeText.innerHTML = "Showing " +employeeCount+ " Employees";
-
-  tableDiv.appendChild(showingEmployeeText);
+function createTable(){
+  var dtable = document.createElement("div");
+  dtable.style.width = "800px";
+  document.body.appendChild(dtable);
+  //var showingEmployeeText = showEmptext();
+  //dtable.appendChild(showingEmployeeText);
 
   // creating the table
   var table = document.createElement("table");
-  table.id = "employeesTable";
+  table.id = "empTable";
   table.style.width = "100%";
-  table.style.background = primaryColor;
+  table.style.height = "30px";
+  table.style.background = "#408dc7";
   table.style.border = "2px solid white";
   table.style.borderCollapse = "collapse";
-  var row1 = document.createElement("tr");
-  row1.style.color = "white";
-  row1.style.border = "2px solid white";
+  var headRow = document.createElement("tr");
+  headRow.style.color = "white";
+  headRow.style.font = "12px sans-serif";
+  headRow.style.border = "2px solid white";
 
   // table headers
-  for (i = 0; i<inputs.length; i++) {
-    col = document.createElement("th");
-    col.style.width = "25%";
-    col.innerHTML = inputs[i];
-    col.style.textAlign = "left";
-    col.style.border = "1px solid white";
-    row1.appendChild(col);
+  for (i = 0; i<labels.length; i++) {
+    var cols = document.createElement("th");
+    cols.style.width = "25%";
+    cols.innerHTML = labels[i];
+    cols.style.font = "sans-serif";
+    cols.style.textAlign = "left";
+    cols.style.border = "1px solid white";
+    headRow.appendChild(cols);
   }
 
-  var row1col4 = document.createElement("th");
-  row1col4.style.width = "25%";
-  row1col4.style.border = "1px solid white";
-  row1col4.align = "center";
+  var delCol = document.createElement("th");
+  delCol.style.width = "25%";
+  delCol.style.border = "1px solid white";
+  delCol.align = "center";
 
-  row1.appendChild(row1col4);
-  table.appendChild(row1);
+  headRow.appendChild(delCol);
+  table.appendChild(headRow);
 
-  tableDiv.appendChild(table);
+  dtable.appendChild(table);
 }
 
-function displayEmployeesTable() {
-  for (i = 0; i < employees.length; i++) {
-    addTableRow(i);
-  }
-}
+function displayTableData(){
+     for(i = 0; i < employees.length; i++){
+        addEmpRow(i);
+     }  
+}  
 
-function addTableRow(index) {
-  var table = $("employeesTable");
+function addEmpRow(length1){
+    var table = $("empTable");
 
-  // creating cell for employee data
   var row = document.createElement("tr");
-  row.id = employees[index][2] + " row";
-  for (j = 0; j < employees[index].length; j++) {
+  row.id = employees[length1][2] + " row";
+  for (j = 0; j < employees[length1].length; j++) {
     var td = document.createElement("td");
-    td.innerHTML = employees[index][j];
+    td.innerHTML = employees[length1][j];
     td.style.border = "1px solid white";
     td.style.font = "14px arial";
     row.appendChild(td);
   }
 
-  // need a cell to place the delete button in
+ 
   var td = document.createElement("td");
   td.style.border = "1px solid white";
   td.align = "center";
   row.appendChild(td);
 
-  // adding delete button
+
   var button = document.createElement("button");
   button.innerHTML = "Delete";
   button.style.color = "white";
-  button.style.background = primaryColor;
+  button.style.background = "#408dc7";
   button.style.padding = "3px 20px";
   button.style.margin = "5px 0";
-  button.id = employees[index][2]+" btn";
+  button.id = employees[length1][2]+" btn";
   row.lastChild.appendChild(button);
 
-  // alternating row color
-  if (index%2 === 0) {
-    row.style.background = secondary1;
+
+  if (length1%2 === 0) {
+    row.style.background = "#D2DEEE";
   } else {
-    row.style.background = secondary2;
+    row.style.background = "#EAEFF5";
   }
   table.appendChild(row);
-
 }
 
-function deleteEmployee(id, ext) {
-  employees.splice(id, 1);
-  var row = $(ext + " row");
-  console.log("Deleting row: " + row.id);
-  row.remove();
-}
+function addEmp(){
+    var name = $("label0");
+    var title = $("label1");
+    var extension = $("label2"); 
 
-
-function addEmployee() {
-  var name = $(inputs[0]+"Input");
-  var title = $(inputs[1]+"Input");
-  var ext = $(inputs[2]+"Input");
-
-  if (isValidInputs()) {
-    var newEmployee = [name.value, title.value, ext.value];
-    var index = employees.push(newEmployee);
-    console.log("Adding " + newEmployee);
-    addTableRow(index-1);
-    window.alert("New employee added!");
-    name.value = "";
-    title.value = "";
-    ext.value = "";
-  }
-}
-
-function isValidInputs() {
-  var valid = true;
-  for (i = 0; i<inputs.length; i++) {
-    var input = $(inputs[i]+"Input").value;
-    if (isEmpty(input)) {
-      $(inputs[i]+"Error").innerHTML = "Please enter " + inputs[i];
-      valid = false;
-    } else {
-      $(inputs[i]+"Error").innerHTML = "";
+    if(isValid()){
+        var newEmp = [name.value, title.value, extension.value];
+        var newArr = employees.push(newEmp);
+        addEmpRow(newArr - 1);
+        name.value = "";
+        title.value = "";
+        extension.value = "";
     }
-  }
-  return valid;
 }
+
+function deleteEmp(i, j) {
+    employees.splice(i, 1);
+    var row = $(j + " row");
+    row.remove();
+  }
+
+function isValid() {
+    var valid = true;
+    for (i = 0; i<labels.length; i++) {
+      var input = $("label" +[i]).value;
+      if (isEmpty(input)) {
+        $(labels[i]+"Error").innerHTML = "Please enter " + labels[i];
+        valid = false;
+      } else {  
+        $(labels[i]+"Error").innerHTML = "";
+      }
+    }
+    return valid;
+  }
 
 function isEmpty(str) {
-  return (!str || 0 === str.length);
+    return (!str || 0 === str.length);
 }
